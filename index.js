d7d1af554bbd8c3e0f28cac7821f41a95557ddf5
ShutDownMan <jedson_gabriel@hotmail.com>
2020-10-02 15:20:30 -0300

    v0.0.4

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, session } = require('electron')
+const { app, BrowserWindow, session, dialog } = require('electron')
 const path = require('path')
 const dns = require('dns');
 const netList = require('network-list');
